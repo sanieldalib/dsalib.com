153d1b1fc4256f973b7c57f4b3388d4c9a3ac60e
Daniel Salib <danielsalib98@gmail.com>
2021-11-11 11:22:27 -0800

    removing NFTstorage to debug

--- frontend/pages/api/mint/[seed].ts
+++ frontend/pages/api/mint/[seed].ts
@@ -15,12 +15,12 @@ export default async function handler(
 
   // const nftStorageApiKey = process.env.NFT_STORAGE_API_KEY;
 
-  if (!nftStorageApiKey) {
-    res
-      .status(400)
-      .json({ error: "Cannot reach nft.storage. Try again later." });
-    return;
-  }
+  // if (!nftStorageApiKey) {
+  //   res
+  //     .status(400)
+  //     .json({ error: "Cannot reach nft.storage. Try again later." });
+  //   return;
+  // }
 
   if (Array.isArray(seed)) {
     res.status(400).json({ error: "Invalid seed." });
