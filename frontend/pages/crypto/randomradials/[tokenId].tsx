9f56b613fa2b4f54411321dabf8691e629455e0e
Daniel Salib <danielsalib98@gmail.com>
2021-11-12 04:52:07 -0800

    added dedicated api key

--- frontend/pages/crypto/randomradials/[tokenId].tsx
+++ frontend/pages/crypto/randomradials/[tokenId].tsx
@@ -47,7 +47,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async (
   context
 ): Promise<GetStaticPropsResult<RandomRadialStaticProps>> => {
-  const provider = new AlchemyProvider("ropsten");
+  const provider = new AlchemyProvider("ropsten", process.env.ALCHEMY_API_KEY);
   const randomRadials = new Contract(
     contractForChain[3],
     RANDOMRADIALS_ABI,
