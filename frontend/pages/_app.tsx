20786a2a332d19c9e62bc6e5b1852d6847e5029f
Daniel Salib <dsalib@atlassian.com>
2022-03-03 14:39:28 -0800

    removed hotjar and added GA

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,16 +3,9 @@ import type { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
 import "../styles/style.css";
 
-import { hotjar } from "react-hotjar";
-import { useEffect } from "react";
-
 import getLibrary from "../controllers/getLibrary";
 
 function NextWeb3App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    hotjar.initialize(2734449, 6);
-  }, []);
-
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
       <Component {...pageProps} />
